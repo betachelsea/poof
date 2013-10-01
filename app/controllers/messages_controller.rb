74da64e815067aa91bef61e1b764274287d5dc04
beta_chelsea <beta.chelsea@gmail.com>
2013-10-01 12:50:43 +0900

    messages#indexが表示できるよう整備

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,26 @@
 class MessagesController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
 end
