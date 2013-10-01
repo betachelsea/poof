4d601416b3841cafc140865626921db42a1224d3
beta_chelsea <beta.chelsea@gmail.com>
2013-10-01 12:39:15 +0900

    messageのテーブルについて項目追加

--- db/migrate/20130930234900_create_messages.rb
+++ db/migrate/20130930234900_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-
+      t.string :message, null: false    #メッセージ
+      t.integer :limit, null: false     #制限時間
+      t.datetime :opendate, null:false  #開封時間
       t.timestamps
     end
   end
