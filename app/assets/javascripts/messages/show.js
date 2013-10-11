8eeb5b26bfd4a377a61eb004eb191802a952bdc2
beta_chelsea <beta.chelsea@gmail.com>
2013-10-11 17:13:55 +0900

    カウントダウンを実施するjsスクリプト作成

--- app/assets/javascripts/messages/show.js
+++ app/assets/javascripts/messages/show.js
@@ -2,4 +2,17 @@ $(function() {
     var countdownObj = $("#countdown");
     var maxTime = countdownObj.attr("data-time");
     countdownObj.text(maxTime);
+
+    var doCountDown = function(max) {
+        var num = max;
+        setInterval(function() {
+            num = num-1;
+            if (num < 0) {
+                location.replace('/');
+            } else {
+                $("#countdown").text(num);
+            }
+        }, 1000);
+    }
+    doCountDown(maxTime);
 });
